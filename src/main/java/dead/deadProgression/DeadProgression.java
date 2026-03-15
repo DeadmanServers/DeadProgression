8ce93f5213bde7396662b8cdb078b69e5dcfa2eb
Deadman <deadmanservers@gmail.com>
2026-03-14 20:38:25 -0400

    First Upload

---
+++ src/main/java/dead/deadProgression/DeadProgression.java
@@ -0,0 +1,24 @@
+package dead.deadProgression;
+
+import dead.deadProgression.commands.ProgressionDebugCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeadProgression extends JavaPlugin {
+
+    private static DeadProgression INSTANCE;
+    
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+
+        saveDefaultConfig();
+
+        getCommand("/upgradedebug").setExecutor(new ProgressionDebugCommand());
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/dead/deadProgression/commands/ProgressionDebugCommand.java
@@ -0,0 +1,24 @@
+package dead.deadProgression.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class ProgressionDebugCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
+
+        if (!(sender instanceof Player player)) {
+            return true;
+        }
+
+        if (args.length == 0) {
+            player.sendRichMessage("&cError: &e/upgradedebug [arguments to be determined]");
+        }
+
+
+        return false;
+    }
+}
