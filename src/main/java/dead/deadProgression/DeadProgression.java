7854f748a14637fab1ee8e4c083165aff8b3966a
Deadman <deadmanservers@gmail.com>
2026-03-16 15:47:23 -0400

    Updated

--- src/main/java/dead/deadProgression/DeadProgression.java
+++ src/main/java/dead/deadProgression/DeadProgression.java
@@ -1,6 +1,7 @@
 package dead.deadProgression;
 
 import dead.deadProgression.ability.AbilityRegistry;
+import dead.deadProgression.categories.CategoryRegistry;
 import dead.deadProgression.commands.ProgressionDebugCommand;
 import dead.deadProgression.upgrades.UpgradeRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,16 +11,19 @@ public final class DeadProgression extends JavaPlugin {
     public static DeadProgression INSTANCE;
     public static AbilityRegistry abilityRegistry;
     public static UpgradeRegistry upgradeRegistry;
+    public static CategoryRegistry categoryRegistry;
 
     @Override
     public void onEnable() {
         INSTANCE = this;
         abilityRegistry = new AbilityRegistry();
         upgradeRegistry = new UpgradeRegistry();
+        categoryRegistry = new CategoryRegistry();
 
         saveDefaultConfig();
         abilityRegistry.load();
         upgradeRegistry.load();
+        categoryRegistry.load();
 
         getCommand("upgradedebug").setExecutor(new ProgressionDebugCommand());
 
@@ -32,6 +36,9 @@ public final class DeadProgression extends JavaPlugin {
         }
         if (upgradeRegistry != null) {
             upgradeRegistry.save();
+        }
+        if (categoryRegistry != null) {
+            categoryRegistry.save();
         }
     }
 }
